a2e9cb1aefe529c1f7cc3512e2d5d2b0515c1449
Erik Stanovský <erikstan@seznam.cz>
2022-05-12 08:23:18 +0200

    .

--- .github/projekt - a;b,2c.py
+++ .github/projekt - a;b,2c.py
@@ -1,9 +1,10 @@
 #1
 import re
-regex = "[0-9]{2},[01-12],[01-31],[0-9]{3},[0-9]"
+regex = "[0-9]{2}[01-12][0-3][0-9][0-9]{3}[0-9]"
 rodnecislo = input("Zadej rodné cislo")
 
-if re.fullmatch(regex, rodnecislo):
-    print("splňuje")
-else:
-    print("nesplňuje")
+def jeSpravne():
+    if re.fullmatch(regex, rodnecislo):
+        print("správně")
+    else:
+        print("nesprávně")
